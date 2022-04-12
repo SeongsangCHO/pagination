dcd2106f41cc43d0ae1a1c48506797499e3ad553
seongsangcho <liasqui31@gmail.com>
2022-04-12 18:18:31 +0900

    chore: ready for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pagination from "./components/Pagination";
+import Pagination from "./lib/Pagination";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
